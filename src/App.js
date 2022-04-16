1024b5f723d1dac6081f93e08f566153ed2a02b4
sangoscar99 <stqnguyen@ucdavis.edu>
2022-04-15 20:38:51 -0700

    init

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p> Testing
+          </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
