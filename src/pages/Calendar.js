5118649f5fb56f4c2fb0b350e87f8d2c2f8c1549
z19951211 <62072350+z19951211@users.noreply.github.com>
2022-04-17 02:57:26 -0700

    process event

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -8,19 +8,55 @@ import {
 } from "@syncfusion/ej2-react-schedule";
 import "./Calendar.css";
 
+//import userJson from "../userData/user_data.json"
+
+
 import { registerLicense } from "@syncfusion/ej2-base";
 registerLicense(
   "ORg4AjUWIQA/Gnt2VVhhQlFaclhJWHxIe0x0RWFbb1d6cV1MY1xBJAtUQF1hS35Xd0NjXX1WcXZXRmlV"
 );
 
 const Calendar = () => {
+  var userEvents = {};
+  var userEventId = [];
+  function processdata(userEvents) {
+    return function (data) {
+      try {
+        var singleEvent = data.data[0];
+        if (!userEventId.includes(singleEvent.Id)) {
+          userEventId.push(singleEvent.Id);
+
+          console.log(data.data[0]);
+          userEvents[singleEvent.Id] = {
+            Id: data.data[0].Id,
+            Subject: data.data[0].Subject,
+            StartTime: Date.parse(data.data[0].StartTime),
+            EndTime: Date.parse(data.data[0].EndTime),
+            Location: data.data[0].Location,
+          };
+
+        } else {
+          userEventId.splice(userEventId.indexOf(singleEvent.Id));
+          delete userEvents[singleEvent.Id];
+          
+        }
+        //writeJsonFile(userJson, userEvents)
+        console.log(userEvents);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  }
   return (
     <div>
       Calendar
-      <ScheduleComponent>
+      <ScheduleComponent
+        selectedDate={new Date(2019, 0, 10)}
+        allowInline={false}
+        actionComplete={processdata(userEvents)}
+      >
         <Inject services={[Day, Week, Month]} />
       </ScheduleComponent>
-      
     </div>
   );
 };
